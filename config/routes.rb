8b4262914562d4b60b73734eea95e279c4f3e8fb
Ubuntu <ubuntu@ip-172-31-17-225.ap-northeast-1.compute.internal>
2025-09-23 03:28:48 +0000

    一時保存

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "home/about" => "homes#about", as: "about"
   resources :users, only: [:index, :show, :edit, :update]
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
